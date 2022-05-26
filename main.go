949c69ae9d238c500b21e010708bee4578207822
Mooningway <Puddingway@hotmail.com>
2022-05-26 10:56:22 +0800

    remake sql tool

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+)
 
+type Sql struct {
+	DriverName     string
+	DataSourceName string
+}
+
+type Insert struct {
+	sql          *Sql
+	db           *sql.DB
+	table        string
+	columns      []string
+	columnsValue map[string]interface{}
+}
+
+func main() {
+	i := Insert{}
+	i.columns = append(i.columns, `13`)
+	fmt.Println(i)
 }
